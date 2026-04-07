c1fbab71453e27559f3daa9c615a58676405142c
Trevnh <trevorhuynh@hotmail.com>
2026-04-07 03:14:20 +0000

    Fix typo in function name from left to right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,7 @@
  *
  * Return: Pointer to new node or NULL on failure or NULL parent.
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
